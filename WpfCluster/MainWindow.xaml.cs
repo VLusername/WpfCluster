9c7e29cc4f994d9a0c2b5558f7392da68df462b8
Viacheslav Liashuk <viacheslav.mail@gmail.com>
2016-12-07 18:20:16 +0200

    add HK_algorithm and draw simple 2D-cluster

--- WpfCluster/MainWindow.xaml.cs
+++ WpfCluster/MainWindow.xaml.cs
@@ -20,9 +20,73 @@ namespace WpfCluster
     /// </summary>
     public partial class MainWindow : Window
     {
+        private FindClustersAlgorithm findObj;
+        private List<int> percolationClusters;
+
         public MainWindow()
         {
             InitializeComponent();
+            fillButton.IsEnabled = false;
+            clearButton.IsEnabled = false;
         }
+
+        private void drawButton_Click(object sender, RoutedEventArgs e)
+        {
+            canvasField.Children.Clear();
+            fillButton.IsEnabled = true;
+            clearButton.IsEnabled = true;
+
+            // TODO: validate input
+
+            int gridSize = Convert.ToInt32(this.gridSize.Text);
+            double probability = Convert.ToDouble(this.probability.Text);
+
+            findObj = new FindClustersAlgorithm(gridSize, probability);
+            findObj.HoshenKopelmanAlgorithm();
+            int totalClusters = findObj.RelabledGrid();
+            this.percolationClusters = findObj.FindPercolationClusters();
+
+            SolidColorBrush rectBrush = new SolidColorBrush(Colors.Black);
+
+            int[,] grid = this.findObj.grid;
+
+            int squareSizeX = (int)(this.canvasField.Width / grid.GetLength(0));
+            int squareSizeY = (int)(this.canvasField.Height / grid.GetLength(1));         
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    if (grid[i, j] != 0)
+                        rectBrush = new SolidColorBrush(Colors.White);
+
+                    Rectangle cellRect = new Rectangle();
+                    cellRect.Width = squareSizeY - 1;
+                    cellRect.Height = squareSizeX - 1;
+                    cellRect.Fill = rectBrush;
+                    cellRect.Name = "box" + i.ToString() + j.ToString();
+                    Canvas.SetLeft(cellRect, j * squareSizeX);
+                    Canvas.SetBottom(cellRect, i * squareSizeY);
+                    canvasField.Children.Add(cellRect);
+
+                    rectBrush = new SolidColorBrush(Colors.Black);
+                }               
+                if (this.slowMotion.IsChecked == true)
+                    System.Threading.Thread.Sleep(50);
+            } 
+        }
+
+        private void fillButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            canvasField.Children.Clear();
+            fillButton.IsEnabled = false;
+            clearButton.IsEnabled = false;
+        }
+
     }
 }
